4a68c0de5b1eeb306063567b12e8682f51f7e89d
FaresHamoud <Fares.h.1828@gmail.com>
2022-06-13 16:55:01 +0200

    JS opdrachten

---
+++ 02_Programming/Javascript/01_basics/Opdrachten/JS excercise/JS_excercise.js
@@ -0,0 +1,15 @@
+let letOefening = "Let Tekst Drie!"
+console.log(letOefening)
+
+
+
+
+
+// let letOefening = "Let Tekst"
+// console.log(letOefening)
+
+// let letOefening = "Let Tekst Twee!"
+// console.log(letOefening)
+
+
+
