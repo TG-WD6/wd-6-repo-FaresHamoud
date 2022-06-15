8e34b0097742b4d9a4a7c664ee7ea7f11ca69997
FaresHamoud <Fares.h.1828@gmail.com>
2022-06-15 16:04:17 +0200

    update

---
+++ 02_Programming/Javascript/01_basics/Opdrachten/Functions_Opdrachten/Opdracht4_1/script.js
@@ -0,0 +1,79 @@
+
+    // <!-- 4.1.1 -->
+function helloOnClick() {
+    console.log("Hello World");
+}
+
+
+
+
+
+<!-- 4.1.2-3- -->
+function processInput(){
+    let userInput = document.getElementById('inputField').value;
+    console.log(userInput * 2);
+
+}
+
+
+
+// opdracht 4.1   4 ⚠️!!!! 
+// var result = processInput;
+
+// function processInput(){
+//     let userInput = document.getElementById('inputField').value;
+//     console.log(userInput * 2);
+// var result = (processInput);
+// return(result)
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("inputField").oninput = function()
+// {var result = document.getElementById("inputField").value;
+// console.log(result * 2)}
+
+
+// document.getElementById("inputField").oninput = function(){
+//     var answerForInput = document.getElementById("inputField").value;
+//     console.log(inputField * 2)
+// }
+
+// function answerForInput() {
+
+// }
+
+// var UserInput = document.GetElementByID("inputField").value;
+// console.log(UserInput * 2);
+
+// function answerForInput() {
+//     console.log(UserInput * 2);
+// }
+
+// // var result = answerForInput()
+
+
+
