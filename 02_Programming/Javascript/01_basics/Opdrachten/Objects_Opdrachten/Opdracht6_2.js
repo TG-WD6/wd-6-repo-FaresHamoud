0cd9951c879b982ff01937ec6b50f7127cf18ed1
FaresHamoud <Fares.h.1828@gmail.com>
2022-06-27 11:02:52 +0200

    update

--- 02_Programming/Javascript/01_basics/Opdrachten/Objects_Opdrachten/Opdracht6_2.js
+++ 02_Programming/Javascript/01_basics/Opdrachten/Objects_Opdrachten/Opdracht6_2.js
@@ -5,14 +5,14 @@
 
 
 
-class Club {
-    constructor(Naam, Typeclub, Leden) {
-        this.Naam = Naam;
-        this.Typeclub = Typeclub;
-        this.Leden = Leden;
-    }
+// class Club {
+//     constructor(Naam, Typeclub, Leden) {
+//         this.Naam = Naam;
+//         this.Typeclub = Typeclub;
+//         this.Leden = Leden;
+//     }
 
-}
+// }
 
 
 
@@ -22,14 +22,16 @@ class Club {
 // Contactpersoon
 
 
-class Contactinformatie extends Club {
-    constructor(Naam, Adres, Telefoonnummer, Contactpersoon) {
-        super(Naam);
-        this.Adres = Adres;
-        this.Telefoonnummer = Telefoonnummer;
-        this.Contactpersoon = Contactpersoon;
-    }
-}
+// class Contactinformatie extends Club {
+//     constructor(Naam, Adres, Telefoonnummer, Contactpersoon) {
+//         super(Naam);
+//         this.Adres = Adres;
+//         this.Telefoonnummer = Telefoonnummer;
+//         this.Contactpersoon = Contactpersoon;
+//     }
+// }
+
+
 
 
 // Maak een array met 5 verschillende Clubs aan. Itereer door het array heen en log voor elke Club de naam, het telefoonnummer en de contactpersoon.
@@ -51,20 +53,56 @@ class Contactinformatie extends Club {
 //         this.Telefoonnummer = Contactinformatie.Telefoonnummer;
 //         this.Contactpersoon = Contactinformatie.Contactpersoon;
 
-    
+
 //     }
 
 // }
 
 
+class Club {
+    constructor(Naam, Typeclub, Leden) {
+        this.Naam = Naam;
+        this.Typeclub = Typeclub;
+        this.Leden = Leden;
+    }
+}
+
+class Contactinformatie {
+    constructor(Adres, Telefoonnummer, Contactpersoon) {
+        this.Adres = Adres;
+        this.Telefoonnummer = Telefoonnummer;
+        this.Contactpersoon = Contactpersoon;
+
+    }
+}
 
 
 
-// let clubsArray = [
+let contact1 = new Contactinformatie('123straat 34b', 061476345, 'Linda')
+let club1 = new Club('VLD', 'Volleyball', 34, contact1)
 
-//     new Club('mark', 'gym', 34, 'hevenstraat 32', '0613364544', 'Jens'),
+let contact2 = new Contactinformatie('532straat 43a', 061375345, 'Jan')
+let club2 = new Club('RDM', 'jogging', 343, contact2)
 
 
-// ]
+let contact3 = new Contactinformatie('2straat 12c', 06142545, 'Marc')
+let club3 = new Club('Heezen', 'Kayak', 532, contact3)
 
-// console.log(clubsArray);
+let contact4 = new Contactinformatie('ABstraat 5', 064255345, 'Steff')
+let club4 = new Club('XtR', 'voetbal', 723, contact4)
+
+
+let contact5 = new Contactinformatie('V straat 44', 064375345, 'Ruth')
+let club5 = new Club('SuperC', 'cross-fit', 53, contact5)
+
+
+
+
+
+
+let ClubArray = [club1, club2, club3, club4, club5]
+for (let i = 0; i < ClubArray.length; i++) {
+    console.log(ClubArray[i].Naam);
+}
+
+
