c98b6170cbf9a33f9e2b6209c96e585bc729cd21
FaresHamoud <Fares.h.1828@gmail.com>
2022-06-30 16:34:26 +0200

    update

--- 02_Programming/Javascript/eindopdracht/JS_Eindopdracht_Fares/JS_Carousel/script.js
+++ 02_Programming/Javascript/eindopdracht/JS_Eindopdracht_Fares/JS_Carousel/script.js
@@ -1,52 +1,64 @@
-const images = document.querySelectorAll(".slides img");
-const slidesContainer = document.querySelector(".slides-container");
-const slides = document.querySelector(".slides");
+const slides = document.querySelector('.slides');
+const slidesImages = document.querySelectorAll('.slides img');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+// to keep track of where are we now in relation to slides
+let counter = 1;
+
+
+// we need to figure out the size of image so we could use that value and move images with translate x , so they mouve out of view as if they were sliding
+const size = slidesImages[0].clientWidth;
+
+slides.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 
 
-const prevBtn = docuemnt.querySelector(".prev-btn");
-const nextBtn = document.querySelector(".next-btn");
+nextBtn.addEventListener('click', () => {
+
+    if(counter >= slidesImages.length -1) return; // to resolve a bug when switching so fast between slides goes ahead of the transition 
+ 
+    slides.style.transition = "transform 0.4s ease-in-out";
+    counter++
+    // console.log(counter); testing 
+    slides.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', () => {
+
+    if (counter <= 0) return;      // to resolve a bug when switching so fast between slides goes ahead of the transition speed
+    slides.style.transition = "transform 0.3s ease-in-out";
+    counter--;
+    slides.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
 
 
 
-let currentSlide = 1;
-const imageSize = images[0].clientWidth
+// to loop back to start we use an event listener that detects reaching the end of slides , we need to reset the transition efffect on the last slide so it would be a loop instead
 
-
-slides.style.transform = `translateX(${-imageSize}px)`;
-
-
-prevBtn.addEventListener('click', () => 
-{
-    if (currentSlide <=0) return;
-    slides.style.transition = '1s ease-in-out transform'; 
-    currentSlide--;
-    slides.style.transform = `translateX(${-imageSize * currentSlide}px)`;
-}
-)
-
-
-nextBtn.addEventListener('click', () => 
-{
-    slides.style.transition = '1s ease-in-out transform'; 
-    currentSlide++;
-    slides.style.transform = `translateX(${-imageSize * currentSlide}px)`;
-}
-)
-
-
-slides.addEventListener('transitioned', () => {
-    if (images[current].classList.contains('first-slide')){
+slides.addEventListener('transitionend', () => {
+    if (slidesImages[counter].id === "last-slide") {
         slides.style.transition = 'none';
-        currentSlide = images.length -2;
-        slides.style.transform = `translateX(${-imageSize * current}px)`;
+        counter = slidesImages.length - 2;         //   (we created a duplicate images of first and last so we use -2 on the counter)
+        slides.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
-if 
-(images[currentSlide].classList.contains('last-slide')){
-    slides.style.transition = 'none';
-    currentSlide = images.length - currentSlide;
-    slides.style.transform = `translateX(${-imageSize * currentSlide}px)`;
-}
+
+    if (slidesImages[counter].id === "first-slide") {
+        slides.style.transition = 'none';
+        counter = slidesImages.length - counter; 
+        slides.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
 
 
-})
+});
+
+
+
+
+
+// document.querySelector(".btn").onclick = function(){
+//     const h1 = document.querySelector(".title");
+//     console.log(h1.style);
+//     h1.style.backgroundColor = "#00FF00"
+// }
