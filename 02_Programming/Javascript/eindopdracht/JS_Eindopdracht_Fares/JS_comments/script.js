cc2015ab77d4ee933adf5d57bc8ff9f425f3c6b1
FaresHamoud <Fares.h.1828@gmail.com>
2022-07-12 16:38:28 +0200

    update

---
+++ 02_Programming/Javascript/eindopdracht/JS_Eindopdracht_Fares/JS_comments/script.js
@@ -0,0 +1,18 @@
+let repliedTo = referToComment(a){a = getElementsByClassName("user-name").value}
+
+
+
+
+function replyFunction(){
+
+    let userComment = document.getElementsByClassName("text-field")
+    let userCommentNumber = 2
+
+    const newComment = document.createElement("div");
+    newComment.appendChild(userComment);
+    const currentComment = document.getElementsByClassName('comment-container');
+    document.body.insert
+
+}
+
+
