d166d300415e3ee15170283ba86cec2ec8b47165
FaresHamoud <Fares.h.1828@gmail.com>
2022-07-13 11:07:40 +0200

    update

--- 02_Programming/Javascript/eindopdracht/JS_Eindopdracht_Fares/JSON_cards/JSON_cards_2/script.js
+++ 02_Programming/Javascript/eindopdracht/JS_Eindopdracht_Fares/JSON_cards/JSON_cards_2/script.js
@@ -1,20 +1,23 @@
 
-    const fetchPokemon = () => {
-
-        const source = `./pokemon.json-master/pokedex.json`
-        fetch(source)
+    const source = `./pokemon.json-master/pokedex.json`
+    fetch(source)
         .then((res) => {
             return res.json();
         })
         .then((data) => {
-            console.log(data);
-            const pokemon = {};
-            pokemon['name'] = data.name;
-            pokemon['id'] = data.id;
-            console.log(pokemon);
-        });
-    
+            fetchPokemon(data);
+            // console.log(data);
+            // const pokemon = [];
+            // pokemon['name'] = data.name;
+            // pokemon['id'] = data.id;
+        
+        }).catch((error) => {
+            console.log(error);
+            console.log('Failed to load json');
+        })
 
-    
-    };
+
+function fetchPokemon(data){
+    console.log(data[0].type);
+}
 
